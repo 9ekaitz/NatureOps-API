54e054a309335bf7534dc719b5d8894ce34c5355
Unai Rodriguez <unai.rodriguezi@alumni.mondragon.edu>
2022-06-04 16:36:34 +0200

    implementacion inicio

--- src/main/java/eus/natureops/natureops/domain/Achivement.java
+++ src/main/java/eus/natureops/natureops/domain/Achivement.java
@@ -1,10 +1,27 @@
 package eus.natureops.natureops.domain;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @Generated @AllArgsConstructor @NoArgsConstructor
 public class Achivement {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String dexription;
   private String image;
   private String objetivo;
   private boolean enabled;
+  
   private User user;
 }
