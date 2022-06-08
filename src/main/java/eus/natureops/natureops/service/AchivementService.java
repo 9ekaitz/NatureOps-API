9669da98759f188f95ed93a3e0ca313a1bc3b932
Unai Rodriguez <unai.rodriguezi@alumni.mondragon.edu>
2022-06-08 12:04:39 +0200

    merge

--- src/main/java/eus/natureops/natureops/service/AchivementService.java
+++ src/main/java/eus/natureops/natureops/service/AchivementService.java
@@ -3,8 +3,6 @@ package eus.natureops.natureops.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
-import eus.natureops.natureops.domain.Achivement;
 import eus.natureops.natureops.form.AchivementsForm;
 
 @Service
