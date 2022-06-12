1a2beb82766ed70716b03e293ad9d90f14fdc4f8
9ekaitz <56532157+9ekaitz@users.noreply.github.com>
2022-06-12 23:49:27 +0200

    path fixed (#21)

--- src/main/java/eus/natureops/natureops/controller/HomeController.java
+++ src/main/java/eus/natureops/natureops/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-  @RequestMapping(value = { "/", "/login", "/logout", "/register", "/dashboard/**" })
+  @RequestMapping(value = { "/", "/login", "/logout", "/register", "/dashboard/{**}" })
   public String index() {
     return "index.html";
   }
